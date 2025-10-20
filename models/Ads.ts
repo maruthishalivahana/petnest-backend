8aef1f2f11fe3c2dc1e712d6363a19e5e5f63909
maruthishalivahana <maruthikummari553@gmail.com>
2025-10-20 18:54:29 +0530

    added ads model

---
+++ models/Ads.ts
@@ -0,0 +1,64 @@
+import { Document, Schema, model, Types, Model } from 'mongoose';
+
+
+export interface IAdvertisement extends Document {
+    brandName: string;
+    contactEmail?: string;
+    contactNumber?: string;
+    adSpot: 'homepage_banner' | 'sidebar' | 'footer' | 'blog_feature';
+    message?: string;
+    mediaUrl?: string;
+    status: 'pending' | 'active' | 'paused' | 'expired' | 'rejected';
+    startDate?: Date;
+    endDate?: Date;
+    createdByAdminId?: Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+
+const AdvertisementSchema: Schema<IAdvertisement> = new Schema(
+    {
+        brandName: {
+            type: String,
+            required: true
+        },
+        contactEmail: {
+            type: String
+        },
+        contactNumber: {
+            type: String
+        },
+        adSpot: {
+            type: String,
+            enum: ['homepage_banner', 'sidebar', 'footer', 'blog_feature'],
+            required: true
+        },
+        message: {
+            type: String
+        },
+        mediaUrl: {
+            type: String
+        },
+        status: {
+            type: String,
+            enum: ['pending', 'active', 'paused', 'expired', 'rejected'],
+            default: 'pending'
+        },
+        startDate: {
+            type: Date
+        },
+        endDate: {
+            type: Date
+        },
+        createdByAdminId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    { timestamps: true }
+);
+
+const Advertisement: Model<IAdvertisement> = model<IAdvertisement>('Advertisement', AdvertisementSchema);
+
+export default Advertisement;
