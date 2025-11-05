34472c0fbb088ab110f10090709690193a268d86
maruthishalivahana <maruthikummari553@gmail.com>
2025-11-05 13:52:31 +0530

    proper validation while uploading user image like file limit , fileformats etc

--- middlewares/upload.ts
+++ middlewares/upload.ts
@@ -1,4 +1,4 @@
-import multer from "multer";
+import multer, { FileFilterCallback } from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary";
 
@@ -7,9 +7,24 @@ const storage = new CloudinaryStorage({
     params: {
         folder: "petnest", // folder name in cloudinary
         allowed_formats: ["jpg", "jpeg", "png", "webp"],
-    } as any,
+        resource_type: "image",
+    } as any
+
 });
 
-const upload = multer({ storage });
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB file size limit
+    fileFilter: (req, file, cb: FileFilterCallback) => {
+        const allowedMimeTypes = ["image/jpeg", "image/png", "image/webp"];
+        if (allowedMimeTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+
+            cb(new Error("Invalid file type. Only JPEG, PNG, and WEBP are allowed."));
+        }
+    }
+});
 
 export default upload;
